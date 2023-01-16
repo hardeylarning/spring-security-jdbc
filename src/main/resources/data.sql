076fc5be7221917caa300535bbe176597bba108f
hardeylarning <hardeylarning@gmail.com>
2023-01-16 14:06:40 +0100

    spring security with jdbc and datasource

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users (username, password, enabled)
+ values ('user', 'pass', true),
+ ('admin', 'pass', true);
+
+insert into authorities (username, authority)
+values ('user', 'ROLE_USER'), ('admin', 'ROLE_ADMIN');
